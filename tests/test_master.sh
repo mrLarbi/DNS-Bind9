20507e85fe20f554742901a505923dd364e4c467
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-28 19:45:26 +0200

    Added an ip argument to the master test script

--- tests/test_master.sh
+++ tests/test_master.sh
@@ -1,8 +1,11 @@
 role_path=$1
+dns_master_ip=$2
 conf_hostname="yoda"
 conf_domain="jedi"
 ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts
                         \ --extra-vars "hostname=$conf_hostname"
                         \ --extra-vars "domain_name=$conf_domain"
+
+echo "nameserver $dns_ip" > /etc/resolv.conf 
 
 ping -c 4 www.$conf_hostname.$conf_domain
