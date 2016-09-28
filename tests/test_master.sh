6dd4a849b432bf4502ddfa47b9c1c93b49a8ddc6
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-28 19:40:11 +0200

    Add a ping test

--- tests/test_master.sh
+++ tests/test_master.sh
@@ -1,3 +1,8 @@
 role_path=$1
+conf_hostname="yoda"
+conf_domain="jedi"
 ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts
+                        \ --extra-vars "hostname=$conf_hostname"
+                        \ --extra-vars "domain_name=$conf_domain"
 
+ping -c 4 www.$conf_hostname.$conf_domain
