3a626ecc1f81f8d43b45fd64c49365cddeeac7f3
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-28 19:58:04 +0200

    Still fixing ...

--- tests/test_master.sh
+++ tests/test_master.sh
@@ -2,9 +2,9 @@ role_path=$1
 dns_master_ip=$2
 conf_hostname="yoda"
 conf_domain="jedi"
-ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts \ 
-                        --extra-vars "hostname=$conf_hostname" \ 
-                        --extra-vars "domain_name=$conf_domain"
+ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts \
+            --extra-vars "hostname=$conf_hostname" \
+            --extra-vars "domain_name=$conf_domain"
 
 echo "nameserver $dns_ip" > /etc/resolv.conf 
 
