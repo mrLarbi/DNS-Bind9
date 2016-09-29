d09b4d7c6259ae0f5ceeaf4116764cfc62da520d
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-29 14:24:41 +0200

    Add installation of a slave

--- tests/test_master.sh
+++ tests/test_master.sh
@@ -4,8 +4,12 @@ conf_hostname="yoda"
 conf_domain="jedi"
 ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts \
             --extra-vars "hostname=$conf_hostname" \
-            --extra-vars "domain_name=$conf_domain"
-
+            --extra-vars "domain_name=$conf_domain" \
+            
 echo "nameserver $dns_ip" > /etc/resolv.conf 
 
+#test master
 ping -c 4 www.$conf_hostname.$conf_domain
+
+#test forwarders
+ping -c 4 www.google.com
