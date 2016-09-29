d917bca043be7e9817fa37078200e7deaafe41e2
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-29 14:57:45 +0200

    Little fix

--- tests/test_master.sh
+++ tests/test_master.sh
@@ -6,7 +6,7 @@ ansible-playbook $role_path/tests/test_master.yml -i $role_path/tests/hosts \
             --extra-vars "hostname=$conf_hostname" \
             --extra-vars "domain_name=$conf_domain" \
             
-echo "nameserver $dns_ip" > /etc/resolv.conf 
+echo "nameserver $dns_master_ip" > /etc/resolv.conf 
 
 #test master
 ping -c 4 www.$conf_hostname.$conf_domain
