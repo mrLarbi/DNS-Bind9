49087186a2e9241f1adc70cfd9d27dfc574fcd18
mrLarbi <larbiyoucef.mohamed@gmail.com>
2016-09-29 19:17:55 +0200

    Another fix :)

--- tests/test_slave.sh
+++ tests/test_slave.sh
@@ -8,8 +8,7 @@ ansible-playbook $role_path/tests/test_slave.yml -i $role_path/tests/hosts \
             --extra-vars "domain_name=$conf_domain" \
             --extra-vars "master_host=${dns_master_ip}"
             
-echo "nameserver $dns_master_ip" > /etc/resolv.conf
-echo "nameserver $dns_slave_ip" >> /etc/resolv.conf
+echo "nameserver $dns_slave_ip" > /etc/resolv.conf
 
 #test master
 ping -c 4 www.$conf_hostname.$conf_domain
